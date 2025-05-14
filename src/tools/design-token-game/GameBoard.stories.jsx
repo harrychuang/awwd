cbd5f12199cda3b19501f77838d6242e098afcf4
Harry Chuang <harrychuang23@gmail.com>
2025-05-15 00:05:11 +0800

    Updated

--- src/tools/design-token-game/GameBoard.stories.jsx
+++ src/tools/design-token-game/GameBoard.stories.jsx
@@ -3,17 +3,10 @@ import GameBoard from './GameBoard'; // 引入我們剛剛建立的 GameBoard �
 import './style.scss'; // 確保樣式被載入
 
 export default {
-  title: 'Tools/Design Token Game/GameBoard', // 在 Storybook 中的路徑
-  component: GameBoard,
-  // 可以加入 parameters 或 args (如果 GameBoard 需要 props 的話)
+  title: 'Tools/Design Token Game', // 在 Storybook 中的路徑
 };
 
 // 基礎的 Story
 const Template = (args) => <GameBoard {...args} />;
 
-export const DefaultGameBoard = Template.bind({});
-DefaultGameBoard.storyName = 'Default Game Board';
-// 如果 GameBoard 有 props，可以在這裡設定預設值
-// DefaultGameBoard.args = {
-//   someProp: 'someValue',
-// }; 
+export const DesignTokenGame = Template.bind({});
